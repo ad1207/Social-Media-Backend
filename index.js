44313d6d9e4944775d4bc7162d11ead7a9656702
Adithya R <59307022+ad1207@users.noreply.github.com>
2022-08-02 10:53:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use('/api/user',userRoutes)
 app.use('/api/auth',authRoutes)
 app.use('/api/posts',postRoutes)
 
-app.listen(5000,() => {
+app.listen(process.env,PORT||5000,() => {
     console.log("Server running!")
 })
 
