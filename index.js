ff951509548852f01d85e7591ab52a802cff36b7
Adithya R <59307022+ad1207@users.noreply.github.com>
2022-08-02 10:16:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const db = require("./database")
+const cors = require("cors")
+const userRoutes = require("./routes/user")
+const authRoutes = require("./routes/auth")
+const postRoutes = require("./routes/posts")
+const multer = require("multer")
+const path = require("path")
+
+dotenv.config()
+db.connect()
+app.use(cors())
+app.use(express.json())
+app.use("/images",express.static(path.join(__dirname, "public/images")));
+const storage = multer.diskStorage({
+    destination:(req,file,cb) => { 
+       cb(null,'public/images') 
+    },
+    filename:(req,file,cb) => {
+        cb(null,req.body.name)
+    },
+})
+
+const upload = multer({storage});
+app.post("/api/upload", upload.single("file"), (req,res) => {
+    
+    try{
+        return res.status(200).json("File uploaded successfully!")
+    }catch(err){
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
+
+
+app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
+app.use('/api/posts',postRoutes)
+
+app.listen(5000,() => {
+    console.log("Server running!")
+})
+
---
+++ routes/auth.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const router = express.Router()
+const User = require("../models/user")
+const bcrypt = require("bcrypt")
+
+
+router.post('/register',async (req,res,next) => {
+    try{
+        const encryptedPassword = await bcrypt.hash(req.body.password,10)
+        const newUser = new User({
+            username:req.body.username,
+            email:req.body.email,
+            password:encryptedPassword
+        })
+        const user = await newUser.save()
+        res.status(200).send(user)
+    }catch(err){
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
+router.post('/login',async (req,res,next) =>{
+    try{
+        const user = await User.findOne({email: req.body.email})
+        !user && res.status(404).json("User not found")
+
+        const correctPassword = await bcrypt.compare(req.body.password,user.password)
+        !correctPassword && res.status(400).json("Incorrect password")
+
+        res.status(200).send(user)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
+router.get('/',(req,res,next) => {
+    res.send("This is auth route")
+})
+
+module.exports = router
